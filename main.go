fcbbfda04b5b62ed54dd6542561a2d9f1dce3d5e
rusisg <rasul.nulrybekov@gmail.com>
2023-09-03 15:45:13 +0600

    divided into packages

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rusisg/rest-api-gin/handler"
+)
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(ctx *gin.Context) {
-
-	})
-
+	r.GET("/albums", handler.GetAlbums)
 	r.Run(":8080")
 }
