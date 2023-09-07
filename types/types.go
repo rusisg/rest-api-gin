923c5e63bcbcda4d387fd9a31c9d2aa180ba7c89
rusisg <rasul.nulrybekov@gmail.com>
2023-09-08 00:48:54 +0600

    made it easy for updating data in the future

--- types/types.go
+++ types/types.go
@@ -7,7 +7,7 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-func NewAlbum() []album {
+func NewAlbum() *[]album {
 	var albums = []album{
 		{ID: "1", Title: "Jitak", Artist: "Rasul Nurlybekov", Price: 20.21},
 		{ID: "2", Title: "Luffy be my hoe", Artist: "Nami onepiece", Price: 14.12},
@@ -15,5 +15,5 @@ func NewAlbum() []album {
 		{ID: "4", Title: "Shart", Artist: "Rusisg", Price: 42},
 		{ID: "5", Title: "SlaTt", Artist: "Playboi Carti", Price: 12.12},
 	}
-	return albums
+	return &albums
 }
